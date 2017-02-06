b1044f3b0b9cafef2b099624fae66f36c636511b
Joseph Tate <joseph@crunch.io>
2017-02-06 15:49:16 -0500

    Add s3 settings lookup to bootstrap script

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -ex
 
-GIT_BRANCH=master
-NEXT_SCRIPT=stage2.sh
-HOSTNAME_ARGS="-2 --domain-tag=Domain --prefix-tag=aws:autoscaling:groupName"
+#Settings needed to bootstrap and load settings
+GIT_BRANCH=${GIT_BRANCH:-master}
 PIP_ARGS=""
+SETTINGS_URL=s3://crunchio-autoscale/bootstrap_ubuntu.profile
 
-#Other ENV Args
+# Can be set here, or loaded from s3 settings
+NEXT_SCRIPT=${NEXT_SCRIPT:-stage2.sh}
+DEBUG=true
+
+#Other ENV Args useful for pre-tasks
 DEBIAN_FRONTEND=noninteractive
 
 #Update all local packages
@@ -16,17 +20,78 @@ PKGS="python-virtualenv curl"
 
 apt-get install -qy $PKGS
 
-
 #Create a virtualenv
 virtualenv VENV
 . VENV/bin/activate
 
-mkdir -p swarmy
-curl -sL https://github.com/Crunch-io/swarmy/tarball/master | tar -xz --strip-components=1 -C swarmy
+if [ ! -d swarmy ]
+then
+    mkdir -p swarmy
+    curl -sL https://github.com/Crunch-io/swarmy/tarball/master | tar -xz --strip-components=1 -C swarmy
+fi
 
 cd swarmy
 python setup.py develop $PIP_ARGS
 
-if [ -f $NEXT_SCRIPT ]; then
-    exec $NEXT_SCRIPT
+function download_next
+{
+    # Usage: FILE=$(download_next URL [tmpfile_suffix])
+    # FILE will be set to the local path of the file
+    # Calling method is responsible for deleting the file
+
+    OUT=$(mktemp --suffix="$2" bootstrap_ubuntu.XXXXXXXXXX) || { echo "Failed to create temp file"; exit 1; }
+    case "$1" in
+      s3://*)
+        #Download the script, set perms, and then execute
+        # AWSCLI is a req of swarmy and is installed during setup.py develop above
+        aws s3 cp $1 $OUT
+        ;;
+      http*://*)
+        #Download the script using curl, then execute
+        curl -sL $1 > $OUT
+        ;;
+      *)
+        cat $1 > $OUT
+        ;;
+    esac
+
+    echo -n $OUT
+}
+
+# Load environment settings from URL
+if [ -n "$SETTINGS_URL" ]; then
+    SETTINGS_URL=${BUCKET_URL:-s3://crunchio-autoscale/.profile}
+
+    SSS=$(download_next $SETTINGS_URL .profile)
+
+    set +e
+
+    source $SSS
+
+    #clean up
+    if [ -z "$DEBUG" ]; then
+        rm -f $SSS
+    fi
+    set -e
+else
+    if [ -n "$DEBUG" ]; then
+        echo "No settings sourced."
+    fi
 fi
+
+# Call the next script
+if [ -f $NEXT_SCRIPT ]; then
+    SCRIPT=$(download_next $NEXT_SCRIPT .stage2.sh)
+    chmod a+x $SCRIPT
+
+    $SCRIPT
+
+    if [ -z "$DEBUG" ]; then
+        rm -f $SCRIPT
+    fi
+else
+    if [ -n "$DEBUG" ]; then
+        echo "No stage 2 provided."
+    fi
+fi
+
