c02dba9ffe9de492c6fe3b339f633dc7472c0713
gjergji <gjergji@crunch.io>
2019-10-10 15:05:40 +0200

    generalize the script and recieving the paramters from profile

--- scripts/addingAdminKeystoLC.sh
+++ scripts/addingAdminKeystoLC.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-groupadd crunch -g 1002
-useradd -m -s /bin/bash -c "Consistent user Crunch" -u 1002 -g 1002 crunch
-echo "crunch ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-mkdir -p /home/crunch/.ssh
-curl http://dev.crunch.io/.admkeys/admkeys --output /tmp/keys
-cat /tmp/keys >> /home/crunch/.ssh/authorized_keys
-USER_NAME=(ec2-user centos ubuntu)
+#adding new user to LC and adding keys.
+
+adduser -m -s /bin/bash --uid $uid $username
+echo "$username ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+mkdir -p /home/$username/.ssh
+curl https://dev.crunch.io/public/admkeys --output /tmp/keys
+cat /tmp/keys >> /home/$username/.ssh/authorized_keys
+
+#adding admin keys to existing users via LC
+
+USER_NAME=($username1 $username2 $username3)
 for i in ${USER_NAME[@]} ; do
     if grep -q "$i" /etc/passwd; then
       cat /tmp/keys >> /home/${USER_NAME}/.ssh/authorized_keys
-    else
+    else 
         echo "User $i Not Found"
     fi
 done
