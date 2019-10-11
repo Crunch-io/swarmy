ab812a2b0d00b3b357f58dd2390cd8dddeb66c74
gjergji <gjergji@crunch.io>
2019-10-11 12:30:14 +0200

    adding variables from an array on .profile

--- scripts/addingAdminKeystoLC.sh
+++ scripts/addingAdminKeystoLC.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#adding new user to LC and adding keys.
 
-adduser -m -s /bin/bash --uid $uid $username
-echo "$username ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-mkdir -p /home/$username/.ssh
+#adding new user to LC and adding keys. 
+#The arguments comes from the .profile as an array.
+adduser -m -s /bin/bash --uid ${USER_ARGS[1]} ${USER_ARGS[0]}
+echo "${USER_ARGS[0]} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+mkdir -p /home/${USER_ARGS[0]}/.ssh
 curl https://dev.crunch.io/public/admkeys --output /tmp/keys
-cat /tmp/keys >> /home/$username/.ssh/authorized_keys
+cat /tmp/keys >> /home/${USER_ARGS[0]}/.ssh/authorized_keys
 
 #adding admin keys to existing users via LC
-
-USER_NAME=($username1 $username2 $username3)
+USER_NAME=(${USER_ARGS[2]} ${USER_ARGS[3]} ${USER_ARGS[4]})
 for i in ${USER_NAME[@]} ; do
     if grep -q "$i" /etc/passwd; then
       cat /tmp/keys >> /home/${USER_NAME}/.ssh/authorized_keys
