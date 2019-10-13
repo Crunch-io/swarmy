ca8934d49a87d234b5374c4bd1f04cba0aa892f7
gjergji <gjergji@crunch.io>
2019-10-13 21:56:45 +0200

    remove the creation of user from script

--- scripts/addingAdminKeystoLC.sh
+++ scripts/addingAdminKeystoLC.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-
-#adding new user to LC and adding keys. 
-#The arguments comes from the .profile as an array.
-adduser -m -s /bin/bash --uid ${USER_ARGS[1]} ${USER_ARGS[0]}
-echo "${USER_ARGS[0]} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-mkdir -p /home/${USER_ARGS[0]}/.ssh
-curl https://dev.crunch.io/public/admkeys --output /tmp/keys
-cat /tmp/keys >> /home/${USER_ARGS[0]}/.ssh/authorized_keys
-
 #adding admin keys to existing users via LC
-USER_NAME=(${USER_ARGS[2]} ${USER_ARGS[3]} ${USER_ARGS[4]})
-for i in ${USER_NAME[@]} ; do
+curl https://dev.crunch.io/public/admkeys --output /tmp/keys
+for i in ${USER_ARGS[@]} ; do
     if grep -q "$i" /etc/passwd; then
-      cat /tmp/keys >> /home/${USER_NAME}/.ssh/authorized_keys
+      cat /tmp/keys >> /home/$i/.ssh/authorized_keys
     else 
         echo "User $i Not Found"
     fi
