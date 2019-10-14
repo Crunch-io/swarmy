fd75847c6dae0296a2dd3473e85160a20cc71e76
gjergji <gjergji@crunch.io>
2019-10-14 22:01:43 +0200

    modify the script to deposit the keys in temp file

--- scripts/addingAdminKeystoLC.sh
+++ scripts/addingAdminKeystoLC.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #adding keys to existing users via LC
-curl ${URL_KEYS[@]} --output /tmp/keys
+tmpfile=$(mktemp) || { echo "Failed to create temp file"; exit 1; }
+curl ${URL_KEYS[@]} --output $tmpfile
 for i in ${USER_ARGS[@]} ; do
     if grep -q "$i" /etc/passwd; then
-      cat /tmp/keys >> /home/$i/.ssh/authorized_keys
-    else 
+      cat $tmpfile >> /home/$i/.ssh/authorized_keys
+    else
         echo "User $i Not Found"
+	rm -f $tmpfile
     fi
 done
