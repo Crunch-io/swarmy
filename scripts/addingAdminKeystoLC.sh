3adf50e7ad54dc8f81eb1d54b5664d3bda6a58c8
gjergji <gjergji@crunch.io>
2019-10-14 10:18:07 +0200

    retrieve keys url from profile

--- scripts/addingAdminKeystoLC.sh
+++ scripts/addingAdminKeystoLC.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#adding admin keys to existing users via LC
-curl https://dev.crunch.io/public/admkeys --output /tmp/keys
+#adding keys to existing users via LC
+curl ${URL_KEYS[@]} --output /tmp/keys
 for i in ${USER_ARGS[@]} ; do
     if grep -q "$i" /etc/passwd; then
       cat /tmp/keys >> /home/$i/.ssh/authorized_keys
